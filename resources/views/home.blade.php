9d7d6ec5c343ccfc7f8134223bb209a7d750875b
eliudarango <eliudarango023@gmail.com>
2024-02-26 23:54:23 -0600

    v4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,11 @@
 @extends('layouts.sidebar')
 
 @section('content')
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -9,19 +14,14 @@
                     <!-- Titulo del card body -->
                     <div class="card-header">{{ __('BIENVENIDO! ') }}{{ Auth::user()->name }}</div>
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
                         <p>
                             <strong>Sabías que:</strong>
                         </p>
                         <!-- Texto del card body -->
                         {{ __('El departamento de Desarrollo Académico del Instituto Tecnológico de Oaxaca desempeña un papel crucial
-                                            al planear, coordinar, controlar y evaluar las actividades destinadas al crecimiento y mejora del personal docente. 
-                                            Su función principal consiste en asegurar que el desarrollo académico de los profesores se lleve a cabo de acuerdo 
-                                            con las normas y lineamientos establecidos por la Secretaría de Educación Pública.') }}
+                                                                    al planear, coordinar, controlar y evaluar las actividades destinadas al crecimiento y mejora del personal docente. 
+                                                                    Su función principal consiste en asegurar que el desarrollo académico de los profesores se lleve a cabo de acuerdo 
+                                                                    con las normas y lineamientos establecidos por la Secretaría de Educación Pública.') }}
                     </div>
                 </div>
             </div>
